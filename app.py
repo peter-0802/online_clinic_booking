8423ac5276ba14da3919e0bec9ada6a5d97bc1d7
Peter Jay Imboy <peterjayimboy@gmail.com>
2022-07-29 23:36:49 +0800

    Added Login Page

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, Response, Blueprint, jsonify
 from flask import render_template
+from datetime import timedelta
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret123'
+app.permanent_session_lifetime = timedelta(hours = 1)
 
 @app.route('/')
 def home():
@@ -11,9 +14,18 @@ def home():
 def tab():
 	return render_template('main.html')
 
-@app.route('/login')
+# Login
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
-	return render_template('login.html')
+	if request.method == "POST":
+		username = request.form['username']
+		password = request.form['pass']
+		if username == 'admin' and password == '123':
+			return redirect(url_for('home'))
+		else:
+			return render_template('login.html')
+	else:
+		return render_template('login.html')
 
 if __name__ == '__main__':
 	app.run(debug=True, host = '0.0.0.0')
